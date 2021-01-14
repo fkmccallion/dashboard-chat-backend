244eeb876e19fca6e4d55cedfcae6ca7eea21340
github username <fkmccallion@gmail.com>
2021-01-14 07:02:40 -0800

    add chat serializer

---
+++ app/serializers/chat_serializer.rb
@@ -0,0 +1,14 @@
+class ChatSerializer
+
+  def initialize(chat_object)
+    @chat = chat_object
+  end
+
+  def to_serialized_json
+    options = {
+      except: [:updated_at, :created_at],
+    }
+    @chat.to_json(options)
+  end
+
+end
