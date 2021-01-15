c1a246ec21ed9d60107d1ef721384056235ff14d
github username <fkmccallion@gmail.com>
2021-01-15 14:23:47 -0800

    update create action

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,4 +5,10 @@ class ChatsController < ApplicationController
     render json: ChatSerializer.new(chats).to_serialized_json
   end
 
+  def create
+    Chat.create(user_id: params[:user_id], content: params[:content])
+    chats = Chat.all
+    render json: ChatSerializer.new(chats).to_serialized_json
+  end
+
 end
